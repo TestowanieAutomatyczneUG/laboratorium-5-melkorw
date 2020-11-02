faf4c30a1ae3425c19de91c143a24a54dc29c2f1
Olek <aleksander.wardyn@jit.team>
2020-11-02 16:06:09 +0100

    Add solution for test 5

--- src/zad1/zad1.py
+++ src/zad1/zad1.py
@@ -8,6 +8,12 @@ class Hamming():
             return 1
         if len(first) == len(second) and first == second:
             return 0
+        if len(first) == len(second) and first != second:
+            wyn = 0
+            for i in range(0, len(first)):
+                if first[i] != second[i]:
+                    wyn += 1
+            return wyn
 
 
 hamming  = Hamming()
