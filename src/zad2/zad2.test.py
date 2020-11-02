6134613f0bd242f54d9da90c9f6c6cd8f5139ffe
Olek <aleksander.wardyn@jit.team>
2020-11-02 17:46:01 +0100

    Enable test_3000_is_three_m_s

--- src/zad2/zad2.test.py
+++ src/zad2/zad2.test.py
@@ -59,7 +59,6 @@ class RomanNumeralsTest(unittest.TestCase):
     def test_1000_is_a_single_m(self):
         self.assertEqual(self.temp.roman(1024), "MXXIV")
 
-    @unittest.skip("demonstrating skipping")
     def test_3000_is_three_m_s(self):
         self.assertEqual(self.temp.roman(3000), "MMM")
 
