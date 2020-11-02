223eecb61c97049a749f44d054dc9afd574c9fae
Olek <aleksander.wardyn@jit.team>
2020-11-02 16:11:44 +0100

    Add solutio for test 7

--- src/zad1/zad1.py
+++ src/zad1/zad1.py
@@ -2,6 +2,9 @@ class Hamming():
     def distance(self, first, second):
         if len(first) > len(second):
             raise ValueError('First cannot be longer than second')
+        
+        if len(first) < len(second):
+            raise ValueError('Second cannot be longer than first')
 
         if len(first) == len(second) and first == second:
             return 0
