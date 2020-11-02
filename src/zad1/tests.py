ad8f8d52b61891293a66f55fc91e47ed2c9ee6d1
Olek <aleksander.wardyn@jit.team>
2020-11-02 16:01:43 +0100

    Enable test 3

--- src/zad1/tests.py
+++ src/zad1/tests.py
@@ -19,7 +19,6 @@ class HammingTest(unittest.TestCase):
     def test_single_letter_identical_strands(self):
         self.assertEqual(self.temp.distance("A", "A"), 0)
 
-    @unittest.skip("demonstrating skipping")
     def test_single_letter_different_strands(self):
         self.assertEqual(self.temp.distance("G", "T"), 1)
 
