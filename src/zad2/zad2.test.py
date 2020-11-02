62a8ae45bcb27bf4cbc82b151d0adb1d4da5beea
Olek <aleksander.wardyn@jit.team>
2020-11-02 17:27:19 +0100

    enable test_49_is_not_40_5_4_but_rather_50_10_10_1

--- src/zad2/zad2.test.py
+++ src/zad2/zad2.test.py
@@ -32,7 +32,6 @@ class RomanNumeralsTest(unittest.TestCase):
     def test_48_is_not_50_2_but_rather_40_8(self):
         self.assertEqual(self.temp.roman(48), "XLVIII")
 
-    @unittest.skip("demonstrating skipping")
     def test_49_is_not_40_5_4_but_rather_50_10_10_1(self):
         self.assertEqual(self.temp.roman(49), "XLIX")
 
