3f6281c4bcd92b2de408b5e88f13971e49bf6284
Olek <aleksander.wardyn@jit.team>
2020-11-02 17:32:31 +0100

    enable test_90_being_100_10_is_xc

--- src/zad2/zad2.test.py
+++ src/zad2/zad2.test.py
@@ -38,7 +38,6 @@ class RomanNumeralsTest(unittest.TestCase):
     def test_50_is_a_single_l(self):
         self.assertEqual(self.temp.roman(59), "LIX")
 
-    @unittest.skip("demonstrating skipping")
     def test_90_being_100_10_is_xc(self):
         self.assertEqual(self.temp.roman(93), "XCIII")
 
