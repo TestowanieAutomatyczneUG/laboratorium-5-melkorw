a185e3dc00f6e74b3aea8aee97448eec10bccce4
Olek <aleksander.wardyn@jit.team>
2020-11-02 16:07:35 +0100

    Refactor class for tests 1-5

--- src/zad1/zad1.py
+++ src/zad1/zad1.py
@@ -1,13 +1,8 @@
 class Hamming():
     def distance(self, first, second):
-        if first == '' and second == '':
-            return 0
-        if len(first) == 1 and len(second) == 1 and first == second:
-            return 0
-        if len(first) == 1 and len(second) == 1 and first != second:
-            return 1
         if len(first) == len(second) and first == second:
             return 0
+
         if len(first) == len(second) and first != second:
             wyn = 0
             for i in range(0, len(first)):
