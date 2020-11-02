ac787d9697e4cf74dc5c88224d8187f599866a95
Olek <aleksander.wardyn@jit.team>
2020-11-02 17:45:11 +0100

    enable test_1000_is_a_single_m

--- src/zad2/zad2.test.py
+++ src/zad2/zad2.test.py
@@ -56,7 +56,6 @@ class RomanNumeralsTest(unittest.TestCase):
     def test_900_being_1000_100_is_cm(self):
         self.assertEqual(self.temp.roman(911), "CMXI")
 
-    @unittest.skip("demonstrating skipping")
     def test_1000_is_a_single_m(self):
         self.assertEqual(self.temp.roman(1024), "MXXIV")
 
