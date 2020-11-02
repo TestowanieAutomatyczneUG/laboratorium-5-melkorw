55a09d37e482c2cfcbc459a166f7757b5c6b22a6
Olek <aleksander.wardyn@jit.team>
2020-11-02 18:38:13 +0100

    Enable and add solution for test_single_line_print

--- src/zad3/zad3.test.py
+++ src/zad3/zad3.test.py
@@ -12,7 +12,6 @@ class SongTest(unittest.TestCase):
     def assertRaisesWithMessage(self, exception):
         return self.assertRaisesRegex(exception, r".+")
 
-    @unittest.skip("demonstrating skipping")
     def test_single_line_print(self):
         self.assertEqual(self.temp.singleLine(1), 'On the first day of Christmas my true love gave to me: a Partridge in a Pear Tree.')
 
