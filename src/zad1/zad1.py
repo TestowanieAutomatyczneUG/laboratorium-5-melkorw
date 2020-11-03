75b8f2a9b29940c9989304f45166571b62829c60
melkorw <57733401+melkorw@users.noreply.github.com>
2020-11-03 21:24:52 +0100

    Update zad1.py

--- src/zad1/zad1.py
+++ src/zad1/zad1.py
@@ -5,5 +5,5 @@ class Hamming():
         wyn = 0
         for i in range(0, len(first)):
             if first[i] != second[i]:
-                wyn += 2
+                wyn += 1
         return wyn
